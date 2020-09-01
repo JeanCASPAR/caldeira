5a1717a55776ad00fd2d2f4de1313aee4998e975
Jean CASPAR <jean.caspar67610@gmail.com>
2020-09-01 15:41:07 +0200

    Remove old commented code

--- caldeira/src/vulkan/debug.rs
+++ caldeira/src/vulkan/debug.rs
@@ -6,22 +6,15 @@ use ash::vk;
 
 use super::Instance;
 
-// use ash::prelude::VkResult;
-// use ash::version::{EntryV1_0, InstanceV1_0};
-// use std::ffi::CString;
-// use std::mem::transmute;
-// use std::ptr::null;
-// use std::rc::Rc;
-
 unsafe extern "system" fn debug_callback(
     message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
     message_type: vk::DebugUtilsMessageTypeFlagsEXT,
     callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT,
     _user_data: *mut c_void,
 ) -> u32 {
-    /*if message_severity < vk::DebugUtilsMessageSeverityFlagsEXT::WARNING {
-        return vk::FALSE;
-    }*/
+    // if message_severity < vk::DebugUtilsMessageSeverityFlagsEXT::WARNING {
+    //     return vk::FALSE;
+    // }
 
     let message_type = match message_type {
         vk::DebugUtilsMessageTypeFlagsEXT::GENERAL => "GENERAL",
@@ -78,75 +71,14 @@ impl Debug {
         }
     }
 
-    // fn create_debug_utils_messenger_ext(
-    //     instance: &Instance,
-    //     create_info: &vk::DebugUtilsMessengerCreateInfoEXT,
-    //     allocator: Option<&vk::AllocationCallbacks>,
-    // ) -> VkResult<vk::DebugUtilsMessengerEXT> {
-    //     let allocator = allocator
-    //         .map(|alloc| alloc as *const vk::AllocationCallbacks)
-    //         .unwrap_or(null());
-
-    //     let name = CString::new("vkCreateDebugUtilsMessengerEXT").unwrap();
-
-    //     let func = unsafe {
-    //         instance
-    //             .entry
-    //             .get_instance_proc_addr(instance.instance.handle(), name.as_ptr())
-    //             .map(|func| transmute::<_, vk::PFN_vkCreateDebugUtilsMessengerEXT>(func))
-    //     };
-
-    //     let mut debug_messenger = vk::DebugUtilsMessengerEXT::null();
-
-    //     if let Some(func) = func {
-    //         let result = func(
-    //             instance.instance.handle(),
-    //             create_info,
-    //             allocator,
-    //             &mut debug_messenger,
-    //         );
-
-    //         if result == vk::Result::SUCCESS {
-    //             Ok(debug_messenger)
-    //         } else {
-    //             Err(result)
-    //         }
-    //     } else {
-    //         Err(vk::Result::ERROR_EXTENSION_NOT_PRESENT)
-    //     }
-    // }
-
-    // fn destroy_debug_utils_messenger_ext(&mut self, allocator: Option<&vk::AllocationCallbacks>) {
-    //     let allocator = allocator
-    //         .map(|alloc| alloc as *const vk::AllocationCallbacks)
-    //         .unwrap_or(null());
-
-    //     let name = CString::new("vkDestroyDebugUtilsMessengerEXT").unwrap();
-
-    //     let func = unsafe {
-    //         self.instance
-    //             .entry
-    //             .get_instance_proc_addr(self.instance.instance.handle(), name.as_ptr())
-    //             .map(|func| transmute::<_, vk::PFN_vkDestroyDebugUtilsMessengerEXT>(func))
-    //     };
-
-    //     if let Some(func) = func {
-    //         func(
-    //             self.instance.instance.handle(),
-    //             self.debug_utils_messenger,
-    //             allocator,
-    //         );
-    //     }
-    // }
-
     fn populate_debug_messenger_create_info<'a>() -> vk::DebugUtilsMessengerCreateInfoEXTBuilder<'a>
     {
         vk::DebugUtilsMessengerCreateInfoEXT::builder()
             .message_severity(
-                vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE
-                    // | vk::DebugUtilsMessageSeverityFlagsEXT::INFO
+                vk::DebugUtilsMessageSeverityFlagsEXT::ERROR
                     | vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
-                    | vk::DebugUtilsMessageSeverityFlagsEXT::ERROR,
+                    // | vk::DebugUtilsMessageSeverityFlagsEXT::INFO
+                    // | vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE,
             )
             .message_type(
                 vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
@@ -159,7 +91,6 @@ impl Debug {
 
 impl Drop for Debug {
     fn drop(&mut self) {
-        // self.destroy_debug_utils_messenger_ext(None);
         unsafe {
             self.debug_utils
                 .destroy_debug_utils_messenger(self.debug_utils_messenger, None);
